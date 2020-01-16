4719e3cfc31f294f8bf0d48ad9a2d8074d27a0e6
SILKNOW <javiersevillaperis@gmail.com>
2020-01-16 06:39:54 +0100

    Delete I.Code

--- Assets/Infinity Code/Online maps/Scripts/Controls/Base/OnlineMapsControlBaseVectorMesh.cs
+++
@@ -1,7 +0,0 @@
-/*     INFINITY CODE 2013-2019      */
-/*   http://www.infinity-code.com   */
-
-public abstract class OnlineMapsControlBaseVectorMesh : OnlineMapsControlBaseDynamicMesh
-{
-    
-}
--- Assets/Infinity Code/Online maps/Scripts/Editor/Controls/OnlineMapsSpriteRendererControlEditor.cs
+++
@@ -1,9 +0,0 @@
-﻿/*     INFINITY CODE 2013-2019      */
-/*   http://www.infinity-code.com   */
-
-using UnityEditor;
-
-[CustomEditor(typeof(OnlineMapsSpriteRendererControl), true)]
-public class OnlineMapsSpriteRendererControlEditor : OnlineMapsControlBase2DEditor<OnlineMapsSpriteRendererControl>
-{
-}
